0e44e3367a1fba964deed58b156b409770dcf68d
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2022-11-04 16:11:33 +0700

    Check for maximam and minumum elements number

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -13,6 +13,21 @@ namespace Exercise_2
 
         public void input()
         {
+            Console.Clear();
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array  ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 27))
+                {
+                    break;
+                }
+                else
+                    Console.WriteLine("\n Array sholud have minimum 1 and maximam 27 elements. \n");
+            }
+
+
 
         }
         static void Main(string[] args)
